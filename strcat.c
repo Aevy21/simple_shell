773ad90a207203e410bbbc12a6a4781e7c9612e5
S1995l <skhosanasl95@gmail.com>
2023-10-17 17:13:56 -0700

    strcat.c

--- strcat.c
+++ strcat.c
@@ -7,17 +7,20 @@
  *
  * Return: A pointer to the resulting string (same as dest).
  */
-char *_strcat(char *dest, char *src) {
+char *_strcat(char *dest, char *src)
+{
 	int dest_len = 0; /* Initialize a variable to hold the length of the destination string. */
 	int i;
 
 	/* Calculate the length of the destination string. */
-	while (dest[dest_len] != '\0') {
+	while (dest[dest_len] != '\0')
+	{
 		dest_len++;
 	}
 
 	/* Copy characters from the source string to the destination string. */
-	for (i = 0; src[i] != '\0'; i++) {
+	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[dest_len + i] = src[i];
 	}
 
